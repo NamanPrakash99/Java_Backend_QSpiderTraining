60fbccf884b1184a84f7416390b87f8f80bdebef
NamanPrakash99 <namanprakash2580@gmail.com>
2026-02-25 14:54:51 +0530

    Upadte assignment7

--- Assignment7-Task1(25-Feb-26)/src/main/java/com/capgemini/Main.java
+++ Assignment7-Task1(25-Feb-26)/src/main/java/com/capgemini/Main.java
@@ -1,7 +1,8 @@
 package com.capgemini;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @Configuration
 @ComponentScan("com.capgemini")
@@ -9,12 +10,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        AnnotationConfigApplicationContext ioc= new AnnotationConfigApplicationContext(Main.class);
+        ApplicationContext ioc= new AnnotationConfigApplicationContext(Main.class);
 
         PaymentProcess process = ioc.getBean(PaymentProcess.class);
 
         process.makePayment(5000);
 
-        ioc.close();
     }
 }
